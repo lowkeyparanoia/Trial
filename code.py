d66abe985c3bf66a2a9f71b51ba1dd75bb1e85bc
lowkeyparanoia <50324377+lowkeyparanoia@users.noreply.github.com>
2020-04-21 21:48:05 +0530

    Update code.py

--- code.py
+++ code.py
@@ -19,11 +19,12 @@ p=input("Enter the no. of keywords: ")
 for x in p:
             listkword.append(input("Enter the keywords:" ))
 
-            for x in listhandle:
-	          kword=listkword[x]
-	          tweets=api.list_timeline(include_rts=false, owner_id=listhandle[x], exclude_replies=true, count=200)
-	          for x in tweets:
-		y=tweets[x]
-		if y!=kword:
-			tweets.remove(x)
-	          print(tweets)
+for x in listhandle:
+	    kword=listkword[x]
+	    tweets=api.list_timeline(include_rts=false, owner_id=listhandle[x], exclude_replies=true, count=200)
+ for x in tweets:
+	    y=tweets[x]
+	    if y!=kword:
+		tweets.remove(x)
+print(tweets)
+
